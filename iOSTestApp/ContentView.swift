8aa2e25974f7f657ebf232a8f966f3f7b6b09841
drushadrusha <drusha@drusha.me>
2022-03-31 19:36:50 +0400

    Initial Commit

---
+++ iOSTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSTestApp
+//
+//  Created by drusha on 31.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSTestApp/iOSTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestAppApp.swift
+//  iOSTestApp
+//
+//  Created by drusha on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
